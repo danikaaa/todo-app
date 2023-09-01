772e8f225e7699e1216e8ca29889ee813ff301f1
danikaaa <dltndus9563@naver.com>
2023-09-02 00:27:28 +0900

    TodoTemplate make

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoInsert from './components/TodoInsert';
+import TodoTemplate from './components/TodoTemplate';
+
+function App() {
+  return (
+  <TodoTemplate>
+    <TodoInsert/>  
+  </TodoTemplate>
+  );
+}
+
+export default App;
