0fe11e37fe5f18cd75ff207ef02c41302f2f9931
danikaaa <dltndus9563@naver.com>
2023-09-02 17:42:54 +0900

    Todo-app 완성

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,16 +1,18 @@
-import { MdRemoveCircle, MdCheckBoxOutlineBlank, MdCheckBox
- } from "react-icons/md";
+import { MdRemoveCircleOutline, MdCheckBoxOutlineBlank, MdCheckBox} from "react-icons/md";
 import './TodoListItem.scss';
+import cn from 'classnames';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+
+    const {id, text, checked} = todo;
 
     return(
         <div className="TodoListItem">
-            <div className="checkbox">
-                <MdCheckBoxOutlineBlank />
-                <div className="text">할 일</div>
+            <div className={cn('checkbox',{ checked })} onClick={() => onToggle(id)}>
+                {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank />}
+                <div className="text">{text}</div>
             </div>
-            <div className="remove"><MdRemoveCircle /></div>
+            <div className="remove" onClick={()=>onRemove(id)}><MdRemoveCircleOutline /></div>
         </div>
 
     );
